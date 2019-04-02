5596683cf96dae6ae6494160514e508e54990141
Justin Foutts <jmfoutts@gmail.com>
2019-04-02 13:10:00 -0700

    working nicely

--- lib/busy_model.dart
+++ lib/busy_model.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 
 import 'package:scoped_model/scoped_model.dart';
 
+// Lift up the "busy state" so that any descendent Widget may access
 class BusyModel extends Model {
   bool busy = false;
   String reason;
@@ -32,29 +33,13 @@ class BusyModel extends Model {
   }
 }
 
-class BusyWidget extends StatelessWidget {
-  final BusyModel model;
-  BusyWidget(this.model);
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 20.0,
-        height: 20.0,
-        child: const CircularProgressIndicator(),
-      ),
-    );
-  }
-}
-
+// Any descendent of BusyScope may alter or depend on the "busy state"
 class BusyScope extends StatelessWidget {
   final BusyModel model;
-  final Widget busy, child;
+  final Widget child;
 
   BusyScope({
     @required this.model,
-    @required this.busy,
     @required this.child
   });
 
@@ -62,29 +47,55 @@ class BusyScope extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScopedModel<BusyModel>(
       model: model,
-      child: ScopedModelDescendant<BusyModel>(
-        builder: (context, chld, cart) => BusySwitch(model, busy, child),
-      ),
+      child: _BusyScope(child),
     );
   }
 }
 
-class BusySwitch extends StatelessWidget {
-  final BusyModel model;
-  final Widget busy, child;
-
-  BusySwitch(this.model, this.busy, this.child);
+// A desecendent widget allows BusyScope to IgnorePointer high up the Widget tree
+class _BusyScope extends StatelessWidget {
+  final Widget child;
+  _BusyScope(this.child);
 
   @override
   Widget build(BuildContext context) {
+    final model = ScopedModel.of<BusyModel>(context, rebuildOnChange: true);
     return IgnorePointer(
       ignoring: model.busy,
-      child: Stack(
-        children: <Widget>[
-          child,
-          model.busy ? busy : Container(width: 0, height: 0),
-        ].where((Object o) => o != null).toList()
-      ),
+      child: child
+    );
+  }
+}
+
+// While BusyModalBarrier may be lower down the Widget tree
+class BusyModalBarrier extends StatelessWidget {
+  final BusyModel model;
+  final Widget child, progressIndicator;
+  Color color;
+  double opacity;
+  bool dismissible;
+
+  BusyModalBarrier({
+    Key key,
+    @required this.model,
+    @required this.child,
+    this.progressIndicator = const Center(child: CircularProgressIndicator()),
+    this.color = Colors.grey,
+    this.opacity = 0.3,
+    this.dismissible = false,
+  }) : assert(child != null), super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        child,
+        !model.busy ? null : Opacity(
+          child: ModalBarrier(dismissible: dismissible, color: color),
+          opacity: opacity,
+        ),
+        !model.busy ? null : progressIndicator,
+      ].where((Object o) => o != null).toList()
     );
   }
 }
